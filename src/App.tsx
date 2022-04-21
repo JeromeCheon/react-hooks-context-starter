b627c2f08cb19ab9e9d85b118a564d5f8ce785f4
Jerome <cheonjh4572@gmail.com>
2022-04-21 11:10:34 +0900

    Add PlusButton to check if context work properly

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { CountProvider } from './Contexts/Count';
 import CountLabel from './Components/CountLabel';
+import { PlusButton } from './Components/PlusButton';
 
 function App() {
 	return (
 		<CountProvider>
 			<CountLabel />
+			<PlusButton />
 		</CountProvider>
 	);
 }
