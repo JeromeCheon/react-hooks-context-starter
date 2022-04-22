0884aa8a661df422b22736d590a5b78ab6ece4f3
Jerome <cheonjh4572@gmail.com>
2022-04-22 13:04:55 +0900

    Add PersonProps separated file and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { Greet } from './Components/Greet';
 import { Heading } from './Components/Heading';
 import { Input } from './Components/Input';
 import { Oscar } from './Components/Oscar';
+import { Person } from './Components/Person';
 import { Status } from './Components/Status';
 
 function App() {
 	return (
 		<div className='App'>
+			<Person name={{ first: 'me', last: 'mine' }} />
 			<Button
 				handleClick={(event, id) => {
 					console.log('Button clicked', event, id);
