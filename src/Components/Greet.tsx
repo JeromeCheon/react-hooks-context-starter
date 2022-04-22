961fa83446e1b21159600e173447b7a20a871d8c
Jerome <cheonjh4572@gmail.com>
2022-04-22 10:36:28 +0900

    Update Greet compon msgCount props as optional and destructing assignment

--- src/Components/Greet.tsx
+++ src/Components/Greet.tsx
@@ -1,19 +1,20 @@
 // type은 application을 만들 때 사용하고
 // interface는 library를 만들 떄 사용하는 것이 좋다고 한다.
 type GreetProps = {
-  name: string;
-  messageCount: number;
-  isLoggedIn: boolean;
+	name: string;
+	messageCount?: number;
+	isLoggedIn: boolean;
 };
 
 export const Greet = (props: GreetProps): JSX.Element => {
-  return (
-    <div>
-      <h2>
-        {props.isLoggedIn
-          ? `Welcome ${props.name}! You have ${props.messageCount} unread messages`
-          : "Welcome guest"}
-      </h2>
-    </div>
-  );
+	const { messageCount = 0 } = props;
+	return (
+		<div>
+			<h2>
+				{props.isLoggedIn
+					? `Welcome ${props.name}! You have ${messageCount} unread messages`
+					: 'Welcome guest'}
+			</h2>
+		</div>
+	);
 };
