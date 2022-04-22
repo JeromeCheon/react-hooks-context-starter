ad2962b4f648424750fa70f6afee824e21764bfa
Jerome <cheonjh4572@gmail.com>
2022-04-22 11:17:14 +0900

    Add Input compon with event type React.ChangeEvent props without state for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,22 @@ import './App.css';
 import { Button } from './Components/Button';
 import { Greet } from './Components/Greet';
 import { Heading } from './Components/Heading';
+import { Input } from './Components/Input';
 import { Oscar } from './Components/Oscar';
 import { Status } from './Components/Status';
-// import { CountProvider } from './Contexts/Count';
-// import CountLabel from './Components/CountLabel';
-// import { UpdateButton } from './Components/OperatorButton';
 
 function App() {
-	const personName = {
-		first: 'Bruce',
-		last: 'Wayne',
-	};
-
-	const nameList = [
-		{
-			first: 'Bruce',
-			last: 'Wayne',
-		},
-		{
-			first: 'Clark',
-			last: 'Kent',
-		},
-		{
-			first: 'Princess',
-			last: 'Diana',
-		},
-	];
-
 	return (
-		// <CountProvider>
-		// 	<CountLabel />
-		// 	<UpdateButton op={'+'} />
-		// 	<UpdateButton op={'-'} />
-		// </CountProvider>
 		<div className='App'>
 			<Button
-				handleClick={(event) => {
-					console.log('Button clicked', event);
+				handleClick={(event, id) => {
+					console.log('Button clicked', event, id);
+				}}
+			/>
+			<Input
+				value=''
+				handleChange={(event) => {
+					console.log('input change event', event);
 				}}
 			/>
 		</div>
