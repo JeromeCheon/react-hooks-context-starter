975c138164b80a3a695d80299ee468f111a834a1
Jerome <cheonjh4572@gmail.com>
2022-04-22 09:06:13 +0900

    Add Person to practice props feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import "./App.css";
-import { Greet } from "./Components/Greet";
+import './App.css';
+import { Greet } from './Components/Greet';
+import { Person } from './Components/Person';
 // import { CountProvider } from './Contexts/Count';
 // import CountLabel from './Components/CountLabel';
 // import { UpdateButton } from './Components/OperatorButton';
 
 function App() {
-  return (
-    // <CountProvider>
-    // 	<CountLabel />
-    // 	<UpdateButton op={'+'} />
-    // 	<UpdateButton op={'-'} />
-    // </CountProvider>
-    <div className='App'>
-      <Greet name='Jerome' messageCount={10} isLoggedIn={false} />
-    </div>
-  );
+	const personName = {
+		first: 'Bruce',
+		last: 'Wayne',
+	};
+	return (
+		// <CountProvider>
+		// 	<CountLabel />
+		// 	<UpdateButton op={'+'} />
+		// 	<UpdateButton op={'-'} />
+		// </CountProvider>
+		<div className='App'>
+			<Greet name='Jerome' messageCount={10} isLoggedIn={false} />
+			<Person name={personName} />
+		</div>
+	);
 }
 
 export default App;
