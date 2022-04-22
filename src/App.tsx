534e18b921cc1eb6a9547e72c1388f6e85b3d10f
Jerome <cheonjh4572@gmail.com>
2022-04-22 14:45:04 +0900

    Add Counter compon to practice useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,13 @@ import { Button } from './Components/Button';
 import { Container } from './Components/Container';
 import { Input } from './Components/Input';
 import { Person } from './Components/Person';
+import { Counter } from './Components/state/Counter';
 import { User } from './Components/state/User';
 
 function App() {
 	return (
 		<div className='App'>
-			<Person name={{ first: 'me', last: 'mine' }} />
-			<Button
-				handleClick={(event, id) => {
-					console.log('Button clicked', event, id);
-				}}
-			/>
-			<Input
-				value=''
-				handleChange={(event) => {
-					console.log('input change event', event);
-				}}
-			/>
-			<Container style={{ border: '1px solid black', padding: '1rem' }} />
-			<User />
+			<Counter />
 		</div>
 	);
 }
